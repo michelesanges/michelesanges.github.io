9725eeaaa2744571a46dfe451a300dabf3f875a1
Michele Sanges <michele.sanges@gmail.com>
2018-01-05 12:21:17 +0100

    Traccia Massaciuccoli - Lucca

--- photo_array.js
+++ photo_array.js
@@ -5,11 +5,10 @@
         'https://goo.gl/RnRTPf', 'https://goo.gl/5y1Tav', 'https://goo.gl/43BomX', 'https://goo.gl/eQvU5F', 'https://goo.gl/dKeMxe', 'https://goo.gl/2cnyaG', 
         'https://goo.gl/F5RQmM', 'https://goo.gl/qZQGDx', 'https://goo.gl/bmPbWo', 'https://goo.gl/dQSs7L', 'https://goo.gl/tEXg5a', 'https://goo.gl/NSfN2H', 
         'https://goo.gl/ep2fVM', 'https://goo.gl/qv5Kw1', 'https://goo.gl/hmxnMs', 'https://goo.gl/7bw4pK', 'https://goo.gl/aP5ZoK','https://goo.gl/y9Nskt', 
-        'https://goo.gl/Vf16i3', 'https://goo.gl/ZYhmPn', 'https://goo.gl/aekTxR', 'https://goo.gl/TwYqtX',  'https://goo.gl/76saNX'
-    ];
-    
+        'https://goo.gl/Vf16i3', 'https://goo.gl/ZYhmPn', 'https://goo.gl/aekTxR', 'https://goo.gl/TwYqtX',  'https://goo.gl/76saNX'];
+
 	image_array[1] = ['https://goo.gl/ZstqLH', 'https://goo.gl/oHrGXS', 'https://goo.gl/ywdStp', 'https://goo.gl/jYQiEV', 'https://goo.gl/4Lc2aP', 'https://goo.gl/oHHNLb',
 	  'https://goo.gl/HQJwe4', 'https://goo.gl/tRjEq3', 'https://goo.gl/xQErJi', 'https://goo.gl/Fzi1K8', 'https://goo.gl/2DZyAV', 'https://goo.gl/N4kh99', 
-	  'https://goo.gl/mu3fSd', 'https://goo.gl/9HGWNJ', 'https://goo.gl/Nm7394', 'https://goo.gl/aErbjV'
-	];
-// 	image_array[1] = ['https://goo.gl/NnZNYf', 'https://goo.gl/5PKFyf'];
+	  'https://goo.gl/mu3fSd', 'https://goo.gl/9HGWNJ', 'https://goo.gl/Nm7394', 'https://goo.gl/aErbjV'];
+
+	image_array[2] = [];
