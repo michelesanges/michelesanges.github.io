a920762b5bdacd45d393d4a3e7be5ef248313e88
Michele Sanges <michele.sanges@gmail.com>
2022-11-03 16:02:14 +0100

    Aggiunto il tour di Firenze

--- look_mum_singledays_photo_array.js
+++ look_mum_singledays_photo_array.js
@@ -79,4 +79,7 @@
      'Somewhere over the Rainbow'
     ];
 
-    
+    index += 1; // 31 OTTOBRE GIUGNO 2022 - QUELLA VOLTA CHE MISI A DURA PROVA I MIEI CINQUANTADUE ANNI
+ 	image_array[index] = ['https://cutt.ly/wNAFRmM', 'https://cutt.ly/RNAFNO8', 'https://cutt.ly/zNAGg4v', 'https://cutt.ly/lNAGSI0', 'https://cutt.ly/lNAG0OP', 'https://cutt.ly/mNAHoof', 'https://cutt.ly/iNAHWHa', 'https://cutt.ly/4NAHOQN', 'https://cutt.ly/bNAHLl1', 'https://cutt.ly/SNAH2t5', 'https://cutt.ly/uNAJqqi'];
+    image_caption[index] =['Piazza Bad Kissingen - Marina di Massa', 'Viale dei Tigli - Viareggio', 'Piazza dei Miracoli - Pisa', 'Indicazioni per Fucecchio', 'Lungo la ciclopista dell`Arno', 'Ponte Vecchio - Firenze', 'Ponte Vecchio - Firenze', 'Ponte Vecchio - Firenze', 'Ponte Vecchio - Firenze', 'Montecatini Terme', 'Lucca'];
+
--- look_mum_singledays_track_array.js
+++ look_mum_singledays_track_array.js
@@ -82,4 +82,39 @@
 				'30 GENNAIO 2022<br>PORTOFINO',
 				'30-01-2022 - Portofino',
 				"rgb(166, 27, 74)", true, null, null];
-                
+
+
+    index += 1;
+    track_description[index] = "<B>\
+Ore 5:45. Buio. Da lì a poco, i colori dell'alba. Un noiosissimo rettilineo fino a Viareggio. Viale dei Tigli chiuso al traffico automobilistico. Lo sterrato della riserva della Lecciona. Pisa. \
+<p>\
+Analisi di algoritmi di stima del Doppler-Shift e del Doppler-Rate nelle comunicazioni numeriche, recita la tesi.<br>\
+Venticinque anni fa uscivo da Pisa pieno di speranze. Oggi trascorro le mie giornate rinchiuso tra mura sormontate da filo spinato.\
+<p>\
+Raggiungo Pontedera. Ho lavorato in questa città per alcuni mesi, ai tempi in cui andavano di moda i Rapporti di collaborazione coordinata e continuativa. Per gli amici, Co.Co.Co.\
+<p>\
+Ho fame. La borsa frontale è piena di cibo. Vengono in aiuto anche le panetterie e i bar. Non smetterò di mangiare per tutta la restante parte della giornata.\
+<p>\
+Empoli. Montelupo fiorentino. Costeggio l'Arno. In risalita, come i salmoni. Finalmente raggiungo Firenze, percorrendo tutto il lungo Parco delle Cascine.<br>\
+Le ultime centinaia di metri prima di Ponte vecchio, non riesco a passare. Un delirio di gente. Devo scendere dalla bicicletta e spingerla a mano. Sono le 15:30 e io sarei già a posto così. C'è però quel pensiero fisso. Voglio mettermi alla prova. Voglio vedere quale effetto avrà sul mio corpo. Voglio vedere quale effetto avrà sulla mia mente.\
+<p>\
+Alle 16:00, riparto. Percorro a ritroso il Parco delle Cascine e svolto verso Pistoia. La luce inizia a calare. Accendo i fari. Non ho assolutamente idea da dove io stia passando. Seguo la traccia che ha scelto per me Google Maps. Si alternano con continuità stradine di campagna e strade ad alto scorrimento. Prego che gli automobilisti mi vedano.\
+<p>\
+Sono a Poggio a Caiano. Il cellulare è quasi scarico. Ho il pacco batterie ma mi sono dimenticato di portare il cavo Usb. Provo a chiedere in un bar. Mentre il cellulare si ricarica, ne approfitto per ordinare da mangiare. In fasi successive, alcuni avventori entrano e chiedono un bicchiere di vino.<br>\
+Li osservo. Penso che sarebbe meglio una buona bottiglia di vino consumata a casa. Ma forse a casa c'è la moglie. O forse non c'è nessuno.<br>\
+Mi osservano. Cosa penseranno invece loro di me, seduto in un bar con il casco e la torcia frontale?\
+<p>\
+Inizia a fare freddo. Indosso tutti i vestiti che mi sono portato dietro e riparto.<br>\
+Supero Pistoia ed entro a Montecatini Terme. Mi fermo dieci minuti a riposare. Ho dolori al sedere, alle braccia e alle mani. C'è tanta gente in Piazza. Famiglie con bambini e giovani mascherati. Oltrepasso pure Lucca e inizio a prendere coscienza che posso farcela. Devo però ancora superare una collinetta. Sono solo centocinquanta metri di elevazione, ma nelle condizioni in cui sono diventano insormontabili. La strada è completamente buia. La percorro a passo d'uomo, sembra non finire più. E' tutto così surreale.<br>\
+Arrivo in cima alla salita, non so bene come. Una lunga discesa ed eccomi a Viareggio. Mi fermo in passeggiata. Ho bisogno di camminare. Spingo la bici per qualche centinaio di metri.\
+<p>\
+Davanti a me, di nuovo quel noiosissimo rettilineo. Fuori da tutti i locali della Versilia, gruppi di giovani mascherati in attesa di festeggiare Halloween.\
+<p>\
+Ore 02:15. Apro il cancello di casa e appoggio la bicicletta. Entro in bagno, mi spoglio dei vestiti buttandoli per terra, mi siedo sfinito sul piatto doccia e mi faccio scorrere l'acqua bollente addosso. Rifletto così sulle ultime venti ore. <br>\
+L'effetto sul corpo è di avere raggiunto una condizione limite di estrema stanchezza. L'effetto sulla mente è di esaltazione totale, quasi come avere partecipato ad un rave party con più di cinquanta persone.\
+    </B>"
+    track_filename[index] = new Array();
+	track_filename[index][0] = ['./dataset/tracks/2022-10-31 - quella volta che misi a dura prova i miei cinquantadue anni.geojson',
+				'31 OTTOBRE 2022<br>QUELLA VOLTA CHE MISI A DURA PROVA I MIEI CINQUANTADUE ANNI',
+				'31-10-2022 - Quella volta che misi a dura prova i miei cinquantadue anni',
+				"rgb(166, 27, 74)", true, null, null];
